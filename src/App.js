ef0f768a61e4e184c5c11104a3e38eaf15b9af6c
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2021-02-01 23:06:24 -0800

    added components and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Grid from '@material-ui/core/Grid';
+import Nav from "./components/Nav/index"
+import { makeStyles } from '@material-ui/core/styles';
+import MainPage from "./pages/Main";
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+        flexGrow: 1,
+
+    },
+    navBar: {
+        zIndex: 1,
+    },
+
+}));
+
 
 function App() {
-  return (
-   <div>
-       
-   </div>
-  );
+
+    const classes = useStyles();
+
+    return (
+
+        <Router>
+
+            <Grid container className={classes.root} >
+
+                <Grid item xs={12} className={classes.navBar}></Grid>
+                <Nav />
+
+            </Grid>
+
+            <Switch>
+
+                <Route exact path="/" component={MainPage}>
+                </Route>
+
+            
+            </Switch>
+
+        </Router>
+
+    );
 }
 
 export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+        
+    },
+    info: {
+        marginTop: 10, 
+    },
+    buttonsGrid: {
+       
+        marginTop: 10,
+
+    }, 
+}));
+
+export default function MainPage() {
+   
+    const classes = useStyles();
+
+    return (
+        <Grid container className={classes.root}>
+
+        </Grid>
+
+    )
+}
